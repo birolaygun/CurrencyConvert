194d6ae3342878dd461230b236b40bc8305a5a29
birol aygün <82452663+birolaygun@users.noreply.github.com>
2022-04-25 09:11:16 +0300

    Update Ratio.js

--- components/Ratio.js
+++ components/Ratio.js
@@ -19,63 +19,68 @@ const Ratio = (props) => {
   const [now, setNow] = useState([]);
 
   useEffect(() => {
-    setShow(mostUsed.filter((fl) => fl != props.from.value && fl != props.to.value));
+    if (props.now) {
+      setShow(
+        mostUsed.filter((fl) => fl != props.from.value && fl != props.to.value)
+      );
 
-    setNow(
-      Object.entries(props.now).map((nw) => ({
-        currency: nw[0],
-        value: nw[1],
-      }))
-    );
+      setNow(
+        Object.entries(props.now).map((nw) => ({
+          currency: nw[0],
+          value: nw[1],
+        }))
+      );
+    }
   }, [props.from, props.now, props.to]);
 
   return (
     <div className="rounded-lg ">
       <div className="hidden md:block shadow-card rounded-lg">
         <div className="h-6 rounded-t-lg bg-gray-100"></div>
-
-        <table className="w-full rounded-lg  " border="1">
-          <thead className=" bg-gray-100">
-            <tr className=" sm:text-lg font-bold rounded-lg">
-              <td></td>
-              <td className=" pb-5 text-center ">1 Day</td>
-              <td className=" pb-5 text-center ">1 Week</td>
-              <td className=" pb-5 text-center ">1 Mount</td>
-              <td className=" pb-5 text-center ">6 Mounts</td>
-              <td className=" pb-5 text-center ">1 Years</td>
-            </tr>
-          </thead>
-          <tbody className="">
-            <RatioItem
-              i={"a"}
-              from={props.from}
-              to={props.to.value}
-              history={Object.entries(props.history)}
-              now={now}
-              otherValues={false}
-            />
-            <RatioItem
-              i={"b"}
-              from={props.from}
-              to={props.to.value}
-              history={Object.entries(props.history)}
-              now={now}
-              otherValues={true}
-            />
-
-            {show.map((listItem, i) => (
+        {props.history && (
+          <table className="w-full rounded-lg  " border="1">
+            <thead className=" bg-gray-100">
+              <tr className=" sm:text-lg font-bold rounded-lg">
+                <td></td>
+                <td className=" pb-5 text-center ">1 Day</td>
+                <td className=" pb-5 text-center ">1 Week</td>
+                <td className=" pb-5 text-center ">1 Mount</td>
+                <td className=" pb-5 text-center ">6 Mounts</td>
+                <td className=" pb-5 text-center ">1 Years</td>
+              </tr>
+            </thead>
+            <tbody className="">
               <RatioItem
-                i={i}
-                key={i}
+                i={"a"}
                 from={props.from}
-                to={listItem}
+                to={props.to.value}
+                history={Object.entries(props.history)}
+                now={now}
+                otherValues={false}
+              />
+              <RatioItem
+                i={"b"}
+                from={props.from}
+                to={props.to.value}
                 history={Object.entries(props.history)}
                 now={now}
                 otherValues={true}
               />
-            ))}
-          </tbody>
-        </table>
+
+              {show.map((listItem, i) => (
+                <RatioItem
+                  i={i}
+                  key={i}
+                  from={props.from}
+                  to={listItem}
+                  history={Object.entries(props.history)}
+                  now={now}
+                  otherValues={true}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
 
       <Carousel
@@ -95,48 +100,50 @@ const Ratio = (props) => {
         <div className="rounded-lg pb-10">
           <div className=" md:hidden  rounded-lg">
             <div className="h-6 rounded-t-lg bg-gray-100"></div>
-            <table className="w-full rounded-lg " border="1">
-              <thead className=" bg-gray-100">
-                <tr className=" sm:text-lg font-bold rounded-lg ">
-                  <td></td>
-                  <td className=" pb-5 text-center ">1 Day</td>
-                  <td className="  pb-5 text-center ">1 Week</td>
-                </tr>
-              </thead>
-              <tbody>
-                <RatioItem
-                  i={"c"}
-                  from={props.from}
-                  to={props.to.value}
-                  history={Object.entries(props.history)}
-                  now={now}
-                  otherValues={false}
-                  carousel={1}
-                />
-                <RatioItem
-                  i={"d"}
-                  from={props.from}
-                  to={props.to.value}
-                  history={Object.entries(props.history)}
-                  now={now}
-                  otherValues={true}
-                  carousel={1}
-                />
-
-                {show.map((listItem, i) => (
+            {props.history && (
+              <table className="w-full rounded-lg " border="1">
+                <thead className=" bg-gray-100">
+                  <tr className=" sm:text-lg font-bold rounded-lg ">
+                    <td></td>
+                    <td className=" pb-5 text-center ">1 Day</td>
+                    <td className="  pb-5 text-center ">1 Week</td>
+                  </tr>
+                </thead>
+                <tbody>
                   <RatioItem
-                    i={i + 25}
-                    key={i}
+                    i={"c"}
                     from={props.from}
-                    to={listItem}
+                    to={props.to.value}
+                    history={Object.entries(props.history)}
+                    now={now}
+                    otherValues={false}
+                    carousel={1}
+                  />
+                  <RatioItem
+                    i={"d"}
+                    from={props.from}
+                    to={props.to.value}
                     history={Object.entries(props.history)}
                     now={now}
                     otherValues={true}
                     carousel={1}
                   />
-                ))}
-              </tbody>
-            </table>
+
+                  {show.map((listItem, i) => (
+                    <RatioItem
+                      i={i + 25}
+                      key={i}
+                      from={props.from}
+                      to={listItem}
+                      history={Object.entries(props.history)}
+                      now={now}
+                      otherValues={true}
+                      carousel={1}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            )}
           </div>
         </div>
 
@@ -144,49 +151,51 @@ const Ratio = (props) => {
           <div className="rounded-lg pb-10">
             <div className=" md:hidden  rounded-lg">
               <div className="h-6 rounded-t-lg bg-gray-100"></div>
-              <table className="w-full rounded-lg " border="1">
-                <thead className=" bg-gray-100">
-                  <tr className=" sm:text-lg font-bold ">
-                    <td className=" pb-5 text-center ">1 Mount</td>
-                    <td className=" pb-5 text-center ">6 Mounts</td>
-                    <td className=" pb-5 text-center ">1 Years</td>
-                    <td></td>
-                  </tr>
-                </thead>
-                <tbody>
-                  <RatioItem
-                    i={"e"}
-                    from={props.from}
-                    to={props.to.value}
-                    history={Object.entries(props.history)}
-                    now={now}
-                    otherValues={false}
-                    carousel={2}
-                  />
-                  <RatioItem
-                    i={"f"}
-                    from={props.from}
-                    to={props.to.value}
-                    history={Object.entries(props.history)}
-                    now={now}
-                    otherValues={true}
-                    carousel={2}
-                  />
-
-                  {show.map((listItem, i) => (
+              {props.history && (
+                <table className="w-full rounded-lg " border="1">
+                  <thead className=" bg-gray-100">
+                    <tr className=" sm:text-lg font-bold ">
+                      <td className=" pb-5 text-center ">1 Mount</td>
+                      <td className=" pb-5 text-center ">6 Mounts</td>
+                      <td className=" pb-5 text-center ">1 Years</td>
+                      <td></td>
+                    </tr>
+                  </thead>
+                  <tbody>
                     <RatioItem
-                      i={i + 50}
-                      key={i}
+                      i={"e"}
                       from={props.from}
-                      to={listItem}
+                      to={props.to.value}
+                      history={Object.entries(props.history)}
+                      now={now}
+                      otherValues={false}
+                      carousel={2}
+                    />
+                    <RatioItem
+                      i={"f"}
+                      from={props.from}
+                      to={props.to.value}
                       history={Object.entries(props.history)}
                       now={now}
                       otherValues={true}
                       carousel={2}
                     />
-                  ))}
-                </tbody>
-              </table>
+
+                    {show.map((listItem, i) => (
+                      <RatioItem
+                        i={i + 50}
+                        key={i}
+                        from={props.from}
+                        to={listItem}
+                        history={Object.entries(props.history)}
+                        now={now}
+                        otherValues={true}
+                        carousel={2}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              )}
             </div>
           </div>
         </div>
